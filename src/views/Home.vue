2b03d95c6c8c938c4251d646248812e74c6a8fc1
Leon <lmshimizu@gmail.com>
2021-11-11 11:18:28 -0800

    Init + index function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ message }}
+    <h1>Contacts:</h1>
+    {{ birthdays }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from 'axios'
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data: function() {
+    return {
+      message: "Hello World",
+      birthdays: []
+    }
   },
+  created: function() {
+    this.indexFunction();
+  },
+  methods: {
+    indexFunction: function() {
+      console.log("in the index function");
+      axios
+        .get('http://localhost:3000/birthdays')
+        .then(response => {
+          console.log(response.data);
+          this.birthdays = response.data;
+        })
+    }
+  }
 };
 </script>
